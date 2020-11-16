b1015bc5e68a72964d609479354d8b0adf4e033f
Mattxm <mattxm@live.com>
2020-11-16 12:55:18 -0800

    Updated code to account for unit testing

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -14,9 +14,105 @@ class PracticeTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-TEST(PracticeTest, is_simple_palindrome)
+TEST(PracticeTest, is_not_palindrome)
 {
     Practice obj;
     bool actual = obj.isPalindrome("Not a palindrome");
+    ASSERT_FALSE(actual);
+}
+
+TEST(PracticeTest, is_simple_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("racecar");
     ASSERT_TRUE(actual);
 }
+
+TEST(PracticeTest, is_empty_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_oneletter_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("a");
+    ASSERT_TRUE(actual);
+}
+
+
+TEST(PracticeTest, is_bad_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("$!@#  #@!$");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, desc_def)
+{
+    Practice obj;
+	int first = 1;
+	int second = 2;
+	int third = 3;
+	obj.sortDescending(first,second,third);
+    bool actual = (first >= second) && (second >= third);
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, desc_bad)
+{
+    Practice obj;
+	int first = 1;
+	int second = 1;
+	int third = 1;
+	obj.sortDescending(first,second,third);
+    bool actual = (first >= second) && (second >= third);
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, desc_two)
+{
+    Practice obj;
+	int first = 1;
+	int second = 1;
+	int third = 2;
+	obj.sortDescending(first,second,third);
+    bool actual = (first >= second) && (second >= third);
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, desc_sorted_already)
+{
+    Practice obj;
+	int first = 3;
+	int second = 2;
+	int third = 1;
+	obj.sortDescending(first,second,third);
+    bool actual = (first >= second) && (second >= third);
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, desc_negative)
+{
+    Practice obj;
+	int first = -3;
+	int second = 2;
+	int third = -1;
+	obj.sortDescending(first,second,third);
+    bool actual = (first >= second) && (second >= third);
+    ASSERT_TRUE(actual);
+}
+
+
+/*
+if((first > second) && (second > third))
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+ */
